e185a52e832266e52851b36e040134acdce9dad0
Miguel Mestre <mestremn28@hotmail.com>
2023-11-20 15:01:10 +0000

    lab7week1day

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require('express');
+const app = express();
+
+const logger = require('morgan')
+
 
 
 
@@ -13,13 +18,36 @@
 // - `express.static()` to serve static files from the `public` folder
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
+app.use(express.static('public'))
+app.use(express.json())
+app.use(logger('dev'))
 
+const projects = require('./data/articles.json')
+const articles = require('./data/projects.json')
 
 
 // ROUTES
 // Start defining your routes here:
 
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/home.html')
+})
 
+app.get('/blog', (req, res)=>{
+    res.sendFile(__dirname + '/views/blog.html')
+})
+
+app.get('/api/articles', (req, res)=>{
+    res.json(projects)
+})
+app.get('/api/projects', (req, res)=>{
+    res.json(articles)
+})
+
+app.get('/*', (req, res)=>{
+    res.status(404).sendFile(__dirname + '/views/not-found.html')
+})
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, () => console.log("nice app"))
