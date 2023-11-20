343f4cb43fcc6a95bad3fd1f0ef38cd232743265
Filipe Dias <dias.filipe2000gmail.com>
2023-11-20 17:49:12 +0000

    express-basic-server lab done

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
 
+// Require Express - same thing as import axios from "axios";
+const express = require("express");
 
+// Import a logger
+const logger = require("morgan");
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
 
-
+// Power on our Express Server
+const app = express(); // calling the express const from before
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
@@ -14,12 +19,40 @@
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
 
+// Make public files accessible
+app.use(express.static("public"));
 
+// Setup our server to use JSON
+app.use(express.json());
+
+// Setup our request logger in our express server
+app.use(logger("dev"));
 
 // ROUTES
 // Start defining your routes here:
 
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/views/home.html");
+});
 
+
+app.get("/blog", (req, res)=>{
+    res.sendFile(__dirname + "/views/blog.html");
+});
+
+app.get("/api/projects", (req, res)=>{
+    res.sendFile(__dirname + "/data/projects.json");
+});
+
+app.get("/api/articles", (req, res)=>{
+    res.sendFile(__dirname + "/data/articles.json");
+});
+
+app.get("/*", (req, res)=>{
+    res.status(404).sendFile(__dirname + "/views/not-found.html");
+});
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+
+app.listen(5005, ()=> console.log("Server listening on port 5005"));
