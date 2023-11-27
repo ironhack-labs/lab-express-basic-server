e3b5f0cdbd093f10a5c7000740a0fca2cdf5665b
Isabel Robleda <isabel.robleda@gmail.com>
2023-11-27 15:25:31 +0100

    addition of about-me, projects and artciles

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ app.use(logger("dev"))
 // Start defining your routes here:
 
 app.get("/", (req, res, next) => {
-    res.send("this is homepage")
-    // res.sendFile(__dirname + "/views/home.html")
+   res.sendFile(__dirname + "/views/home.html")
 })
 
 app.get("/blog", (req, res, next) => {
@@ -33,11 +32,83 @@ app.get("/blog", (req, res, next) => {
 })
 
 app.get("/api/projects", (req, res, next) => {
-    res.json(__dirname + "/data/projects.json")
+
+    const projects = [
+        {
+            "title": "Blackjack Game",
+            "description": "Simple blackjack game, built with vanilla JavaScript, HTML, and CSS.",
+            "image": "https://placehold.co/600x400/4265E6/FFFFFF.png?text=Project+Screenshot",
+            "github": "https://github.com/exampleuser/examplerepo",
+            "deployed": "http://example.com",
+            "techStack": ["HTML", "CSS", "JavaScript"]
+        },
+        {
+            "title": "Jira Clone",
+            "description": "Bootcamp group project. Full stack application built with React, JSON Server, and React Testing Library",
+            "image": "",
+            "github": "https://github.com/exampleuser/examplerepo",
+            "deployed": "http://example.com",
+            "techStack": ["React", "Node", "JSON Server", "React Testing Library"]
+        },
+        {
+            "title": "Cohort Tools",
+            "description": "Bootcamp group project. Full stack application built with React, Node, ExpressJS,  MongoDB, and Tailwind.",
+            "image": "",
+            "github": "https://github.com/exampleuser/examplerepo",
+            "deployed": "http://example.com",
+            "techStack": ["React", "Node", "Express", "MongoDB", "Tailwind"]
+        },    
+        {
+            "title": "Weather Dashboard",
+            "description": "Weather tracking application built with React, Node, ExpressJS, MongoDB, Ant Design, and OpenWeather API.",
+            "image": "",
+            "github": "https://github.com/exampleuser/examplerepo",
+            "deployed": "http://example.com",
+            "techStack": ["React", "Node", "Express", "MongoDB", "Ant Design"]
+        }
+    ]
+
+    res.json(projects)
 })
 
 app.get("/api/articles", (req, res, next) => {
-    res.json(__dirname + "/data/articles.json")
+
+    const articles = [
+        {
+          "title": "The best article ever",
+          "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+          "author": "Jane Doe",
+          "publishedAt": "2022-01-08",
+          "tags": ["react", "node", "testing", "react testing library"],
+          "views": 16
+        },
+        {
+          "title": "The second best article ever",
+          "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+          "author": "Jane Doe",
+          "publishedAt": "2022-03-24",
+          "tags": ["javascript", "node", "express"],
+          "views": 234
+        },
+        {
+          "title": "The third best article ever",
+          "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+          "author": "Jane Doe",
+          "publishedAt": "2022-05-14",
+          "tags": ["javascript", "css", "tailwind"],
+          "views": 345
+        },
+        {
+          "title":"The fourth best article ever",
+          "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+          "author": "Jane Doe",
+          "publishedAt": "2023-02-02",
+          "tags": ["javascript", "css", "testing", "jasmine"],
+          "views": 128
+        }
+      ]
+      
+    res.json(articles)
 })
 
 app.get("/*", (req, res, next) => {
