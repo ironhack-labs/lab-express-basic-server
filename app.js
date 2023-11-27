e57707c6461a70d82e389510ebecb502a0d51314
Huseyin Hepyalniz <huseyinhepyalniz@huseyins-air.speedport.ip>
2023-11-27 14:10:02 +0100

    Espress, Morgan packages imported. Listen on port seted. Middleware created.

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
 
+const express = require ('express');
+const logger = require ('morgan');
+
 
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
+const app = express();
 
 
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
 // - `express.static()` to serve static files from the `public` folder
+app.use(express.static('public'));
 // - `express.json()` to parse incoming requests with JSON payloads
+app.use(express.json());
 // - `morgan` logger to log all incoming requests
+app.use(morgan("dev"));
 
 
 
@@ -23,3 +30,6 @@
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+
+app.listen(5005, () => console.log("Listen on port 5005"));
+
