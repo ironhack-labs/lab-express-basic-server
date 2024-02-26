f264615ca3dcde754cd8f299504426b0d8044fa9
Stefan Kummerlöw <gummiz@gmx.net>
2024-02-26 14:51:23 +0100

    feat(app.js): add required packages for Express app (express, morgan) to improve functionality and logging feat(app.js): add routes for home, blog, JSON data endpoints to provide content for the app feat(app.js): add 404 route to handle unknown routes and display a custom error page feat(app.js): make Express server listen on port 5005 to run the app on a specific port fix(package.json): add required dependencies (express, morgan) to ensure proper functioning of the app feat(views/home.html): create HTML template for the home page, including profile image and project section feat(views/blog.html): create HTML template for the blog page, including fetching and displaying articles dynamically feat(views/not-found.html): create HTML template for the 404 error page, providing a user-friendly error message and a link to go back to the home page feat(public/styles.css): add CSS styles for the app, including colors, button styles, navbar, tags, gradients, and error messages feat(data/projects.json): update project data with actual projects, including titles, descriptions, images, GitHub links, deployed links, and tech stack feat(public/huetopia.png): add project image for Huetopia feat(public/profile-s.png): add profile image for the home page feat(public/profile.png): add profile image for the blog page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
-
-
+const express = require("express")
+const morgan = require("morgan")
+const projects = require("./data/projects.json")
+const articles = require("./data/articles.json")
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
-
+const app = express()
 
 
 // MIDDLEWARE
@@ -13,13 +15,40 @@
 // - `express.static()` to serve static files from the `public` folder
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
-
+app.use(express.static("public"))
+app.use(express.json())
+app.use(morgan('dev'))
 
 
 // ROUTES
 // Start defining your routes here:
 
+// home
+app.get("/", (req, res, next) =>{
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+// blog 
+app.get("/blog", (req, res, next) =>{
+    res.sendFile(__dirname + "/views/blog.html")
+})
+
+// JSON Data
+app.get("/api/projects", (req, res, next) => {
+    res.json(projects)
+} )
+
+// articles
+app.get("/api/articles", (req, res, next) => {
+    res.json(articles)
+})
+
+// 404
+app.get("*", (req, res, next) => {
+     res.sendFile(__dirname + "/views/not-found.html")
+})
 
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, () => console.log("Server started @ 5005"))
