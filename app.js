86165fe8a87adec9f95399c91fba6d13a9b080b8
Lara Aguerre <aguerre.lara@gmail.com>
2024-02-26 16:29:04 +0100

    lab done

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
-
-
+const express = require('express')
+const logger = require('morgan')
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
-
+const app = express()
 
 
 // MIDDLEWARE
@@ -13,13 +13,42 @@
 // - `express.static()` to serve static files from the `public` folder
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
+app.use(logger('dev'))
+app.use(express.static('public'))
+app.use(express.json())
 
 
 
 // ROUTES
 // Start defining your routes here:
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/home.html`)
+})
+
+app.get('/blog', (req, res) => {
+    res.sendFile(`${__dirname}/views/blog.html`)
+})
+
+app.get('/api/projects', (req, res) => {
+
+    const projects = require(`${__dirname}/data/projects.json`)
+
+    res.json(projects)
+})
+
+app.get('/api/articles', (req, res) => {
+
+    const articles = require(`${__dirname}/data/articles.json`)
+
+    res.json(articles)
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(`${__dirname}/views/not-found.html`)
+})
 
 
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, () => console.log("Express working on 5005 PORT"))
