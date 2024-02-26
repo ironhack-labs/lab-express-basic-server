ee14e5a5707c19eaef8ad4739d135545599b7337
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-02-26 14:46:31 +0100

    lab done

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
 
+const express = require("express");
+const morgan = require("morgan");
 
+const projects = require("./data/projects.json")
+const articles = require("./data/articles.json")
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
+
+const app = express();
 
 
 
@@ -14,12 +20,39 @@
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(morgan('dev')); // log every request to the console
+
 
 
 // ROUTES
 // Start defining your routes here:
 
+app.get("/home", (req, res, next) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get("/blog", (req, res, next) => {
+    res.sendFile(__dirname + '/views/blog.html')
+})
+
+
+app.get("/api/projects", (req, res, next) => {
+    res.json(projects)
+})
+
+app.get("/api/articles", (req, res, next) => {
+    res.json(articles)
+})
+
+
+app.get("*", (req, res, next) => {
+    res.sendFile(__dirname + '/views/not-found.html')
+})
+
 
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, () => console.log("Server listening on port 5005"));
