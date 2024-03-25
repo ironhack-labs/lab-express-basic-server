52889705f977b5119dc5adce701b837e64e9d94a
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-25 14:54:42 +0100

    iteration 7

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-// IMPORT PACKAGES
-// Here you should import the required packages for your Express app: `express` and `morgan`
 const express = require("express");
 const logger = require("morgan");
-
 const app = express();
+
+const projects = require("./data/projects.json");
+const articles = require("./data/articles.json");
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(logger("dev"));
-
-// MIDDLEWARE
-// Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
-
-// ROUTES
-// Start defining your routes here:
 
 app.get("/", (req, res) => {
   console.log("test");
@@ -26,5 +17,15 @@ app.get("/", (req, res) => {
 app.get("/blog", (req, res) => {
   res.sendFile(__dirname + "/views/blog.html");
 });
+
+app.get("/api/projects", (req, res) => {
+  res.json(projects);
+});
+
+app.get("/api/articles", (req, res) => {
+  res.json(articles);
+});
+
+app.get("*", (req, res) => res.sendFile(__dirname + "/views/not-found.html"));
 
 app.listen(5005, () => console.log("listening on port 5005"));
